c931252f015b31d5763336a2e86c1475d1948aac
Harrison Woods <hbwoodsjr@gmail.com>
2018-02-04 08:32:51 -0500

    Exercice 1 complete

---
+++ ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/* This is a comment. */
+int main (int argc, char* argv[])
+{
+	int distance = 100;
+
+// this is also a comment
+printf("You are %d miles away.\n", distance);
+printf("Here is somw addiional text.\n");
+printf("I live in the state of MD.\n");
+
+return 0;
+}
